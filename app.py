233b0aa56d62dacb4f17a93aff5473994cd29517
LeoJerry20 <leojerry2004@gmail.com>
2024-09-16 19:04:49 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask
+from flask import request
+from flask import Response
+import requests
+ 
+TOKEN ="6362394999:AAHOLbM6azwg8_qAyHpWtgJs8EPgOezNsH4"
+app = Flask(__name__)
+ 
+def parse_message(message):
+    print("message-->",message)
+    chat_id = message['message']['chat']['id']
+    txt = message['message']['text']
+    print("chat_id-->", chat_id)
+    print("txt-->", txt)
+    return chat_id,txt
+ 
+def tel_send_message(chat_id, text):
+    url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+    payload = {
+                'chat_id': chat_id,
+                'text': text
+                }
+   
+    r = requests.post(url,json=payload)
+    return r
+ 
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        msg = request.get_json()
+       
+        chat_id,txt = parse_message(msg)
+        if txt == "/start":
+            tel_send_message(chat_id,"Enter the no of subject:")
+            for i in range(1,txt+1):
+                tel_send_message(chat_id,txt)
+        else:
+            tel_send_message(chat_id,'send /start to calculate gpa')
+       
+        return Response('ok', status=200)
+    else:
+        return "<h1>Welcome!</h1>"
+ 
